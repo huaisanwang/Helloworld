00edd33a88a61ca927110e3e3a451e27fc44eb6f
huaisan.wang <632494788@qq.com>
2014-10-09 21:20:54 +0800

    ddasdfaf

---
+++ HelloSanGe/HelloSanGe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloSanGe
+//
+//  Created by apple on 14-10-9.
+//  Copyright (c) 2014年 huaisan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloSanGe/HelloSanGe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloSanGe
+//
+//  Created by apple on 14-10-9.
+//  Copyright (c) 2014年 huaisan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
